f6bc01e45c35057748cedf1d505bb8f76b30b8af
Simon Zwicker <github@zwicker.io>
2025-01-06 13:08:58 +0100

    Add Dict for Codables

---
+++ Sources/Mammut/Extensions/Codable.swift
@@ -0,0 +1,22 @@
+//
+//  Codable.swift
+//  Mammut
+//
+//  Created by Simon Zwicker on 06.01.25.
+//
+
+import Foundation
+
+extension Encodable {
+	var dictionary: [String: Any] {
+		guard
+			let data = try? JSONEncoder().encode(self),
+			let object = try? JSONSerialization.jsonObject(with: data, options: []),
+			let dict = object as? [String: Any]
+		else {
+			return [:]
+		}
+
+		return dict
+	}
+}
